5843c83367c7566b2691e1bfb1f4cd3892cab646
Oleg Pustynnikov <void656@mail.ru>
2016-02-19 20:54:56 +0300

    add to repo

---
+++ views/orders/_search.php
@@ -0,0 +1,37 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+/* @var $this yii\web\View */
+/* @var $model app\models\search\OrdersSearch */
+/* @var $form yii\widgets\ActiveForm */
+?>
+
+<div class="orders-search">
+
+    <?php $form = ActiveForm::begin([
+        'action' => ['index'],
+        'method' => 'get',
+    ]); ?>
+
+    <?= $form->field($model, 'id') ?>
+
+    <?= $form->field($model, 'name') ?>
+
+    <?= $form->field($model, 'address') ?>
+
+    <?= $form->field($model, 'email') ?>
+
+    <?= $form->field($model, 'phone') ?>
+
+    <?php // echo $form->field($model, 'create_date') ?>
+
+    <div class="form-group">
+        <?= Html::submitButton(Yii::t('app', 'Search'), ['class' => 'btn btn-primary']) ?>
+        <?= Html::resetButton(Yii::t('app', 'Reset'), ['class' => 'btn btn-default']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
